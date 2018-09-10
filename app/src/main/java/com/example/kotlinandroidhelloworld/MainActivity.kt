217d08c13ac02547e0799f594159be345c7a4728
katiecostello <katiecostello@gwmail.gwu.edu>
2018-09-10 18:30:38 -0400

    Converted Java file to Kotlin.

---
+++ app/src/main/java/com/example/kotlinandroidhelloworld/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.kotlinandroidhelloworld
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+/**
+ * Let's convert this class to Kotlin as an example!
+ */
+class MainActivity : AppCompatActivity() {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        updateText("We need to update this code to Kotlin!")
+    }
+
+    private fun updateText(newText: String) {
+        val textView = findViewById<TextView>(R.id.textview)
+        textView.text = newText
+    }
+}
