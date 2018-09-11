57a24f4dc5a53806e2abb9938030d7a789e8738b
AaronCoplan <afcoplan@gmail.com>
2018-09-11 13:24:41 -0400

    Minor indentation fix.

--- app/src/main/java/com/example/kotlinandroidhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/kotlinandroidhelloworld/MainActivity.kt
@@ -9,7 +9,7 @@ import android.widget.TextView
  */
 class MainActivity : AppCompatActivity() {
 
-     public override fun onCreate(savedInstanceState: Bundle?) {
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
